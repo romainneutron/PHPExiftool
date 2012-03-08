ecd4bb471d114ae24ec615659d78c072c65f399b
Romain Neutron <imprec@gmail.com>
2012-03-08 01:20:52 +0100

    Updated license

--- lib/PHPExiftool/Exiftool.php
+++ lib/PHPExiftool/Exiftool.php
@@ -1,13 +1,24 @@
 <?php
 
-/*
- * This file is part of Phraseanet
+/**
+ * Copyright (c) 2012 Romain Neutron
  *
- * (c) 2005-2010 Alchemy
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
+
 
 namespace PHPExiftool;
 
@@ -15,9 +26,8 @@ use \Symfony\Component\Process\Process;
 
 /**
  *
- * @package
- * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
- * @link        www.phraseanet.com
+ * @author      Romain Neutron - imprec@gmail.com
+ * @license     http://opensource.org/licenses/MIT MIT
  */
 class Exiftool
 {
