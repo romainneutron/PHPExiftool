47cf1e48981c8ec715ed54c53417b8e708365287
Romain Neutron <imprec@gmail.com>
2012-03-08 01:05:15 +0100

    Add Doctrine\Common namespace to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,13 @@
 <?php
 
-require __DIR__ . '/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require __DIR__ . '/lib/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespaces(
         array(
             'Symfony'=> __DIR__ . '/lib/vendor/',
             'PHPExiftool'=> __DIR__ . '/lib/',
+            'Doctrine\Common'=> __DIR__ . '/lib/vendor/Doctrine-Common/lib',
             )
         );
 $loader->register();
