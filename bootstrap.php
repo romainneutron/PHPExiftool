0b9bf55c9ceeb566b37e1f536e6941ec76fc7862
Romain Neutron <imprec@gmail.com>
2012-03-13 14:47:46 +0100

    Move dependencies to composer

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,11 @@
 <?php
 
-require __DIR__ . '/lib/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require __DIR__ . '/vendor/.composer/autoload.php';
 
 $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespaces(
         array(
-            'Symfony'=> __DIR__ . '/lib/vendor/',
             'PHPExiftool'=> __DIR__ . '/lib/',
-            'Doctrine\Common'=> __DIR__ . '/lib/vendor/Doctrine-Common/lib',
             )
         );
 $loader->register();
