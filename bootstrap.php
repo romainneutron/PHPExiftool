c73582434fe92e402ecb14808982ae3e6b7f61f0
Romain Neutron <imprec@gmail.com>
2012-03-15 19:38:47 +0100

    Cleanup bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,11 +2,4 @@
 
 require __DIR__ . '/vendor/.composer/autoload.php';
 
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespaces(
-        array(
-            'PHPExiftool'=> __DIR__ . '/lib/',
-            )
-        );
-$loader->register();
 
