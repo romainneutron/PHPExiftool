95fefd3536905ed33c7529f55e53bb98beed6f0c
Romain Neutron <imprec@gmail.com>
2012-04-03 10:52:16 +0200

    Fix CS

--- check_cs.php
+++ check_cs.php
@@ -51,63 +51,63 @@ $count = 0;
 foreach ($finder as $file)
 {
 
-  /* @var $file Symfony\Component\Finder\SplFileInfo */
+    /* @var $file Symfony\Component\Finder\SplFileInfo */
 
-  $old = file_get_contents($file->getRealpath());
+    $old = file_get_contents($file->getRealpath());
 
-  $new = $old;
+    $new = $old;
 
-  // [Structure] Never use short tags (<?)
-  $new = str_replace('<? ', '<?php ', $new);
+    // [Structure] Never use short tags (<?)
+    $new = str_replace('<? ', '<?php ', $new);
 
-  // [Structure] Indentation is done by steps of four spaces (tabs are never allowed)
-  $new = preg_replace_callback('/^( *)(\t+)/m', function ($matches) use ($new)
-    {
-      return $matches[1] . str_repeat('    ', strlen($matches[2]));
-    }, $new);
-
-  // [Structure] Use the linefeed character (0x0A) to end lines
-  $new = str_replace("\r\n", "\n", $new);
-
-  // [Structure] Don't add trailing spaces at the end of lines
-  $new = preg_replace('/[ \t]*$/m', '', $new);
-
-  // [Structure] Convert tabs to spaces
-  $new = preg_replace('/\t/m', '  ', $new);
-
-  // [Structure] Add a blank line before return statements
-  $new = preg_replace_callback('/(^.*$)\n(^ +return)/m', function ($match)
-    {
-      // don't add it if the previous line is ...
-      if (
-        preg_match('/\{$/m', $match[1]) || // ... ending with an opening brace
-        preg_match('/\:$/m', $match[1]) || // ... ending with a colon (e.g. a case statement)
-        preg_match('%^ *//%m', $match[1]) || // ... an inline comment
-        preg_match('/^$/m', $match[1])    // ... already blank
-      )
+    // [Structure] Indentation is done by steps of four spaces (tabs are never allowed)
+    $new = preg_replace_callback('/^( *)(\t+)/m', function ($matches) use ($new)
       {
-        return $match[1] . "\n" . $match[2];
-      }
+          return $matches[1] . str_repeat('    ', strlen($matches[2]));
+      }, $new);
 
-      return $match[1] . "\n\n" . $match[2];
-    }, $new);
+    // [Structure] Use the linefeed character (0x0A) to end lines
+    $new = str_replace("\r\n", "\n", $new);
 
-  // [Structure] A file must always ends with a linefeed character
-  if (strlen($new) && "\n" != substr($new, -1))
-  {
-    $new .= "\n";
-  }
+    // [Structure] Don't add trailing spaces at the end of lines
+    $new = preg_replace('/[ \t]*$/m', '', $new);
 
-  if ($new != $old)
-  {
-    $count++;
+    // [Structure] Convert tabs to spaces
+    $new = preg_replace('/\t/m', '  ', $new);
 
-    if ($fix)
+    // [Structure] Add a blank line before return statements
+    $new = preg_replace_callback('/(^.*$)\n(^ +return)/m', function ($match)
+      {
+          // don't add it if the previous line is ...
+          if (
+            preg_match('/\{$/m', $match[1]) || // ... ending with an opening brace
+            preg_match('/\:$/m', $match[1]) || // ... ending with a colon (e.g. a case statement)
+            preg_match('%^ *//%m', $match[1]) || // ... an inline comment
+            preg_match('/^$/m', $match[1])    // ... already blank
+          )
+          {
+              return $match[1] . "\n" . $match[2];
+          }
+
+          return $match[1] . "\n\n" . $match[2];
+      }, $new);
+
+    // [Structure] A file must always ends with a linefeed character
+    if (strlen($new) && "\n" != substr($new, -1))
     {
-      file_put_contents($file->getRealpath(), $new);
+        $new .= "\n";
     }
-    printf('%4d) %s' . PHP_EOL, $count, $file->getRelativePathname());
-  }
+
+    if ($new != $old)
+    {
+        $count ++;
+
+        if ($fix)
+        {
+            file_put_contents($file->getRealpath(), $new);
+        }
+        printf('%4d) %s' . PHP_EOL, $count, $file->getRelativePathname());
+    }
 }
 
 exit($count ? 1 : 0);
