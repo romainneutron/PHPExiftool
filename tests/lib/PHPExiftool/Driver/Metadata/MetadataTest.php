b3befd301261f6d1ee340091a22cb539b308cff8
Romain Neutron <imprec@gmail.com>
2012-04-19 19:35:18 +0200

    Normalize tests

--- tests/lib/PHPExiftool/Driver/Metadata/MetadataTest.php
+++ tests/lib/PHPExiftool/Driver/Metadata/MetadataTest.php
@@ -2,57 +2,42 @@
 
 namespace PHPExiftool\Driver\Metadata;
 
-require_once dirname(__FILE__) . '/../../../../../lib/PHPExiftool/Driver/Metadata/Metadata.php';
-
-class TagTest extends \PHPExiftool\Driver\Tag
-{
-
-}
-
 class MetadataTest extends \PHPUnit_Framework_TestCase
 {
 
-  /**
-   * @var Metadata
-   */
-  protected $object;
-  protected $tag;
-  protected $value;
+    /**
+     * @var Metadata
+     */
+    protected $object;
+    protected $tag;
+    protected $value;
 
-  /**
-   * Sets up the fixture, for example, opens a network connection.
-   * This method is called before a test is executed.
-   */
-  protected function setUp()
-  {
-    $this->tag = new TagTest();
-    $this->value = 'valeur';
-    $this->object = new Metadata(new TagTest, $this->value, new \SplFileInfo(__FILE__));
-  }
+    protected function setUp()
+    {
+        $this->tag = new TagTest();
+        $this->value = 'valeur';
+        $this->object = new Metadata(new TagTest, $this->value, new \SplFileInfo(__FILE__));
+    }
 
-  /**
-   * @covers \PHPExiftool\Driver\Metadata\Metadata::reset
-   */
-  public function testReset()
-  {
-    $this->object->reset();
-    $this->assertNull($this->object->getValue());
-  }
+    /**
+     * @covers \PHPExiftool\Driver\Metadata\Metadata::getTag
+     */
+    public function testGetTag()
+    {
+        $this->assertEquals($this->object->getTag(), $this->tag);
+    }
 
-  /**
-   * @covers \PHPExiftool\Driver\Metadata\Metadata::getTag
-   */
-  public function testGetTag()
-  {
-    $this->assertEquals($this->object->getTag(), $this->tag);
-  }
+    /**
+     * @covers \PHPExiftool\Driver\Metadata\Metadata::getValue
+     */
+    public function testGetValue()
+    {
+        $this->assertEquals($this->object->getValue(), $this->value);
+    }
 
-  /**
-   * @covers \PHPExiftool\Driver\Metadata\Metadata::getValue
-   */
-  public function testGetValue()
-  {
-    $this->assertEquals($this->object->getValue(), $this->value);
-  }
+}
+
+class TagTest extends \PHPExiftool\Driver\Tag
+{
 
 }
