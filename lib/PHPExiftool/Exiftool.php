40ea80e9ca67c994421d49cd3f525aba49f75d58
Romain Neutron <imprec@gmail.com>
2012-04-20 13:50:46 +0200

    Exiftool is now an abstract class

--- lib/PHPExiftool/Exiftool.php
+++ lib/PHPExiftool/Exiftool.php
@@ -23,125 +23,8 @@ namespace PHPExiftool;
 
 use \Symfony\Component\Process\Process;
 
-/**
- *
- * @author      Romain Neutron - imprec@gmail.com
- * @license     http://opensource.org/licenses/MIT MIT
- */
-class Exiftool
+abstract class Exiftool
 {
-
-    /**
-     * For use with list option
-     */
-
-    const LISTTYPE_WRITABLE          = 'w';
-    /**
-     * For use with list option
-     */
-    const LISTTYPE_SUPPORTED_FILEEXT = 'f';
-    /**
-     * For use with list option
-     */
-    const LISTTYPE_WRITABLE_FILEEXT  = 'wf';
-    /**
-     * For use with list option
-     */
-    const LISTTYPE_SUPPORTED_XML     = 'x';
-    /**
-     * For use with list option
-     */
-    const LISTTYPE_DELETABLE_GROUPS  = 'd';
-    /**
-     * For use with list option
-     */
-    const LISTTYPE_GROUPS            = 'g';
-
-    /**
-     * Return an Entity corresponding to the file
-     *
-     * @param \SplFileInfo $file
-     * @return \PHPExiftool\FileEntity
-     * @throws \LogicException
-     * @throws \Exception
-     */
-    public function read(\SplFileInfo $file)
-    {
-        if ( ! $file->isFile())
-        {
-            throw new \LogicException('Exiftool::read must be used to read files, '
-              . 'if you want to read directories, use Exiftool::readDirectory');
-        }
-
-        $Entities = RDFParser::Parse(static::Xtract($file));
-
-        if ($Entities->count() !== 1)
-        {
-            throw new \Exception('Something went wrong');
-        }
-
-        return $Entities->first();
-    }
-
-    /**
-     * Returns an ArrayCollection of \PHPExiftool\FileEntity found in the
-     * directory
-     *
-     * @param \SplFileInfo $Dir
-     * @param boolean $recursive
-     * @return \Doctrine\Common\Collections\ArrayCollection
-     * @throws \LogicException
-     */
-    public function readDirectory(\SplFileInfo $Dir, $recursive)
-    {
-        if ( ! $Dir->isDir())
-        {
-            throw new \LogicException('Exiftool::readDirectory must be used to read dirs, '
-              . 'if you want to read files, use Exiftool::read');
-        }
-
-        return RDFParser::Parse(static::Xtract($Dir, $recursive));
-    }
-
-    /**
-     * Execute exiftool with a -X option against the file|directory provided
-     * Return the output, a RDF file
-     *
-     * @see http://www.sno.phy.queensu.ca/~phil/exiftool/exiftool_pod.html#item_x
-     * @param \SplFileInfo $file
-     * @param boolean $recursive
-     * @return string
-     */
-    public static function Xtract(\SplFileInfo $file, $recursive = false)
-    {
-        $option = $recursive ? ' -R ' : '';
-
-        $command = self::getBinary() . ' -X ' . $option . escapeshellarg($file->getPathname());
-
-        return static::executeCommand($command);
-    }
-
-    /**
-     * Return the result of a Exiftool -list* command
-     *
-     * @see http://www.sno.phy.queensu.ca/~phil/exiftool/exiftool_pod.html#item__2dlist_2c__2dlistw_2c__2dlistf_2c__2dlistr_2c__2d
-     * @param string $type One of the LISTTYPE_* constants
-     * @return type
-     * @throws \Exception
-     */
-    public static function listDatas($type = self::LISTTYPE_SUPPORTED_XML)
-    {
-        $available = array(
-          self::LISTTYPE_WRITABLE, self::LISTTYPE_SUPPORTED_FILEEXT
-          , self::LISTTYPE_WRITABLE_FILEEXT, self::LISTTYPE_SUPPORTED_XML
-          , self::LISTTYPE_DELETABLE_GROUPS, self::LISTTYPE_GROUPS,
-        );
-
-        if ( ! in_array($type, $available))
-            throw new \Exception('Unknown list attribute');
-
-        return static::executeCommand(self::getBinary() . ' -f -list' . $type);
-    }
 
     /**
      * Execute a command and return the output
@@ -150,7 +33,7 @@ class Exiftool
      * @return string
      * @throws \Exception
      */
-    private static function executeCommand($command)
+    protected static function executeCommand($command)
     {
         /**
          * Windows custom exec
@@ -174,7 +57,7 @@ class Exiftool
 
         if ( ! $process->isSuccessful())
         {
-            throw new \Exception(sprintf('Command %s failed', $command));
+            throw new Exception\RuntimeException(sprintf('Command %s failed', $command));
         }
 
         return $process->getOutput();
@@ -184,7 +67,7 @@ class Exiftool
      *
      * @return string
      */
-    private static function getBinary()
+    protected static function getBinary()
     {
         if (defined('PHP_WINDOWS_VERSION_BUILD'))
         {
--- tests/lib/PHPExiftool/ExiftoolTest.php
+++ tests/lib/PHPExiftool/ExiftoolTest.php
@@ -6,21 +6,48 @@ class ExiftoolTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
-     * @var Exiftool
+     * @covers PHPExiftool\Exiftool::getBinary
      */
-    protected $object;
-
-    protected function setUp()
+    public function testGetBinary()
     {
-        $this->object = new Exiftool;
+        $this->assertTrue(is_executable(ExiftoolTester::getBinaryTester()));
     }
 
     /**
-     * @covers \PHPExiftool\Exiftool::listDatas
+     * @covers PHPExiftool\Exiftool::executeCommand
      */
-    public function testListDatas()
+    public function testExecuteCommand()
     {
-        $this->object->listDatas();
+        $command = ExiftoolTester::getBinaryTester() . ' -ver';
+
+        $this->assertRegExp('/\d+\.\d+/', ExiftoolTester::executeCommandTester($command));
+    }
+
+    /**
+     * @covers PHPExiftool\Exiftool::executeCommand
+     * @covers \PHPExiftool\Exception\RuntimeException
+     * @expectedException \PHPExiftool\Exception\RuntimeException
+     */
+    public function testExecuteCommandFailed()
+    {
+        $command = ExiftoolTester::getBinaryTester() . ' -version';
+        ExiftoolTester::executeCommandTester($command);
     }
 
 }
+
+class ExiftoolTester extends Exiftool
+{
+
+    public static function getBinaryTester()
+    {
+        return self::getBinary();
+    }
+
+    public static function executeCommandTester($command)
+    {
+        return self::executeCommand($command);
+    }
+
+}
+
