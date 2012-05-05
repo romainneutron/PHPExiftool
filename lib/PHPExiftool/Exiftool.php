909526450186ca70b9ed1c419622e5f99ffa206d
Romain Neutron <imprec@gmail.com>
2012-05-06 00:48:03 +0200

    Symfony\Process now takes care of PHP bug

--- lib/PHPExiftool/Exiftool.php
+++ lib/PHPExiftool/Exiftool.php
@@ -25,21 +25,6 @@ abstract class Exiftool
      */
     protected static function executeCommand($command)
     {
-        /**
-         * Windows custom exec
-         * Symfony Process can fail
-         *
-         * @see https://bugs.php.net/bug.php?id=60120
-         * @see https://bugs.php.net/bug.php?id=51800
-         */
-        if (defined('PHP_WINDOWS_VERSION_BUILD')) {
-            if (null === $output = shell_exec($command)) {
-                throw new Exception\RuntimeException(sprintf('Command %s failed', $command));
-            }
-
-            return $output;
-        }
-
         $process = new Process($command);
         $process->run();
 
